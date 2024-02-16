72f2b6d43f5cbf897efad72b9b6ea49d95ce548c
Jin young <xmfkdnak98@gmail.com>
2024-02-17 03:22:54 +0900

    refactor: 변수 이름, 메인메뉴 명령어, 삭제 기능, 조회 기능 리팩토링, startBtn
    변수이름 기능에 맞게 이름변경
    올바르지 않은 명령어 입력 시 다시 입력하시오 메시지  추가
    삭제할 데이터가 없을 시 접근 못하도록 추가
    삭제할 데이터의 번호를 잘 못 입력시 다시 입력하라는 메시지를 띄우게 추가
    조회할 데이터가 없을 시 조회할 데이터가 없습니다 메시지 추가
    todoList()함수 콜백함수로...

--- ToDoList.js
+++ ToDoList.js
@@ -5,33 +5,49 @@ const startBtn = document.getElementById('start-btn');
 const todoData = [];
 
 // 버튼을 누를 시 Todolist 프로그램이 실행된다.
-startBtn.addEventListener('click', function(){
-  todoList();
-});
+startBtn.addEventListener('click', todoList);
 
 function todoList() {
   while(true){
     // 명령어 입력을 prompt()함수로 받는다.
-    const input = prompt("명령어를 입력해주세요!");
+    const command = prompt("명령어를 입력해주세요!");
     
-    if(input === "추가") {
-      const input2 = prompt("todo를 입력해주세요!");
-      todoData.push(input2);
-      console.log(`${input2}을 추가하셨습니다.`);
-    } else if (input === "삭제") {
-      const input3 = prompt("삭제할 todo의 번호를 입력해주세요!");
-      console.log(`${todoData[input3-1]}를 삭제하셨습니다.`);
-      todoData.splice(input3-1, 1);
-    } else if (input === "조회") {
+    if(command === "추가") {
+      const addTodo = prompt("todo를 입력해주세요!");
+      todoData.push(addTodo);
+      console.log(`${addTodo}을 추가하셨습니다.`);
+    } else if (command === "삭제") {
+      if(todoData.length === 0){
+        console.log("삭제할 데이터가 없습니다.")
+      } else {
+        while(true){
+          const deleteTodo = prompt("삭제할 todo의 번호를 입력해주세요!");
+
+          if( 0 < deleteTodo && deleteTodo <= todoData.length) {
+            console.log(`${todoData[deleteTodo-1]}를 삭제하셨습니다.`);
+            todoData.splice(deleteTodo-1, 1);
+            break
+          } else {
+            console.log("번호를 다시 입력해주세요!")
+          }
+        }
+      }
+    } else if (command === "조회") {
       console.log("--------------------");
-      for(let i=0; i<todoData.length; i++) {
-        console.log(`${i+1}.${todoData[i]}`);
+      if(todoData.length === 0 ) {
+        console.log("조회할 데이터가 없습니다.");
+      } else {
+        todoData.forEach(function(todo, index){
+          console.log(`${index +1}. ${todo}`);
+        });
       }
       console.log("--------------------");
-    } else if (input === "종료") {
+    } else if (command === "종료") {
       console.log("종료하셨습니다.");
       break;  
-    }
+    } else {
+      console.log("명령어를 다시 입력해주세요.");
+    };
 
   };
 };
