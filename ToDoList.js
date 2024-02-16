85d70ae80891881df42eeaf101922772cb725500
Jin young <xmfkdnak98@gmail.com>
2024-02-17 02:03:45 +0900

    feat: todo추가, todo조회 기능 추가

--- ToDoList.js
+++ ToDoList.js
@@ -1,11 +1,13 @@
 // 프로그램 시작 버튼
 const startBtn = document.getElementById('start-btn');
 
+// Todo가 들어갈 배열 공간
+const todoData = [];
+
 // 버튼을 누를 시 Todolist 프로그램이 실행된다.
 startBtn.addEventListener('click', function(){
   todoList();
-})
-
+});
 
 function todoList() {
   while(true){
@@ -13,15 +15,21 @@ function todoList() {
     const input = prompt("명령어를 입력해주세요!");
     
     if(input === "추가") {
-      console.log("추가하셨습니다.");
-    }else if(input === "삭제") {
+      const input2 = prompt("todo를 입력해주세요!");
+      todoData.push(input2);
+      console.log(`${input2}을 추가하셨습니다.`);
+    } else if (input === "삭제") {
       console.log("삭제하셨습니다.");
-    }else if(input === "조회") {
-      console.log("조회하셨습니다.")
-    }else if(input === "종료") {
+    } else if (input === "조회") {
+      console.log("--------------------");
+      for(let i=0; i<todoData.length; i++) {
+        console.log(`${i+1}.${todoData[i]}`);
+      }
+      console.log("--------------------");
+    } else if (input === "종료") {
       console.log("종료하셨습니다.");
       break;  
     }
 
-  }
-}
+  };
+};
